440d155ff758f94bf1651dbfe9944ab79ece7e6d
Ken Bolton <ken@bscientific.org>
2012-05-23 20:37:23 -0400

    Remove !next event exception. Add if/else to event list on index.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 exports.index = function (req, res) {
   var next = req.found.shift();
-  if (!next) { throw new Error('Undefined next event'); }
   res.render('index', {
     title: 'Meetdown.js',
     next: next,
